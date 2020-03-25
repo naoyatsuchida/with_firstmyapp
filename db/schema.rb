b8dff3d78c6e4ad7e8045d384eb2344838e13eef
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-03-25 19:20:14 +0900

    f

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_054334) do
     t.string "title"
     t.string "image"
     t.text "body"
-    t.text "memo"
+    t.text "memomemo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
